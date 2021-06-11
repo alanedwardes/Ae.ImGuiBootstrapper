14aa6302c807a13fb556c73b9d8db72edcf549b0
Alan Edwardes <alan@alanedwardes.com>
2021-06-11 22:35:49 +0100

    Fixes the MultipleWindows demo.

--- samples/MultipleWindows/Program.cs
+++ samples/MultipleWindows/Program.cs
@@ -14,16 +14,16 @@ namespace Ae.ImGuiBootstrapper.MultipleWindows
 
             using var window2 = new ImGuiWindow("ImGui.NET Sample Program 2");
 
+            var backgroundColor = new Vector3(0.45f, 0.55f, 0.6f);
+
             while (window1.IsOpen || window2.IsOpen)
             {
-                var backgroundColor = new Vector3(0.45f, 0.55f, 0.6f);
-
-                while (window1.Loop(ref backgroundColor))
+                if (window1.Loop(ref backgroundColor))
                 {
                     ImGui.ShowDemoWindow();
                 }
 
-                while (window2.Loop(ref backgroundColor))
+                if (window2.Loop(ref backgroundColor))
                 {
                     ImGui.Begin("Window 2", ImGuiWindowFlags.AlwaysAutoResize);
                     ImGui.Text("Testing2");
